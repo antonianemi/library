9c22f066ad1852bd2d4ef1dbfa958c715afbf86a
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2018-08-08 22:09:42 -0500

    Show information personal of the user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display the specified resource.
+     *
+     * @param  $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', compact('user'));
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,6 +16,10 @@ use Faker\Generator as Faker;
 $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
+        'middle_name' => $faker->name,
+        'last_name' => $faker->lastName,
+        'mothers_last_name' => $faker->lastName,
+        'date_of_birth' => \Carbon\Carbon::now()->addWeeks(2),
         'email' => $faker->unique()->safeEmail,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
