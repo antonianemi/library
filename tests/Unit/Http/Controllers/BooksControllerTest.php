b5e30aa5b1a6b6c7884560147517b36ac696e24f
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2018-09-12 19:55:30 -0500

    Make book controller plural

--- tests/Unit/Http/Controllers/BooksControllerTest.php
+++ tests/Unit/Http/Controllers/BooksControllerTest.php
@@ -7,7 +7,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class BookControllerTest extends TestCase
+class BooksControllerTest extends TestCase
 {
     use RefreshDatabase;
 
