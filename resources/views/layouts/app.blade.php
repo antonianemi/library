9abadb762e1fdcebf8dc810e768605eb389ed5d0
Arnold Martinez <arnold.martinez.martinez.91@gmail.com>
2018-09-19 18:47:59 -0500

    Wrap the sections tags on the document main

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -79,14 +79,16 @@
     <div class="showcase">
 
     </div>
-    <div id="main" class="group">
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-    <div id="footer">
+    <main id="main" class="py-4">
+        @yield('content')
+        <aside id="sidebar">
 
-    </div>
+        </aside>
+        <footer id="footer">
+
+        </footer>
+    </main>
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
